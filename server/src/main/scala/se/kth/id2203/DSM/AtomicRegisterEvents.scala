26c8a9c4377cea9b9154a57d17f38dff15390e9c
stean4 <stephan.horsthemke@gmail.com>
2019-03-01 11:12:44 +0100

    Finished merging

--- server/src/main/scala/se/kth/id2203/DSM/AtomicRegisterEvents.scala
+++ server/src/main/scala/se/kth/id2203/DSM/AtomicRegisterEvents.scala
@@ -4,9 +4,9 @@ import java.util.UUID
 
 import se.sics.kompics.KompicsEvent;
 
-case class AR_Read_Request(id: UUID) extends KompicsEvent
+case class AR_Read_Request(id: UUID, key: String) extends KompicsEvent
 case class AR_Read_Response(value: Option[Any], id: UUID) extends KompicsEvent
-case class AR_Write_Request(value: Any, id: UUID) extends KompicsEvent
+case class AR_Write_Request(value: Any, key: String, id: UUID) extends KompicsEvent
 case class AR_Write_Response(id: UUID) extends KompicsEvent
 
 //The following events are to be used internally by the Atomic Register implementation below
